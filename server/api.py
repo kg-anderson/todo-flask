79bfe722ffa3594e532df4c57f5d306502c7d3bf
Keith <kg.anderson@outlook.com>
2021-03-25 16:38:06 +1300

    Create flask-sqlalchemy DB

--- server/api.py
+++ server/api.py
@@ -1,8 +1,19 @@
 from flask import Flask
 from flask_cors import CORS
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 CORS(app)
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///example.db"
+db = SQLAlchemy(app)
+
+# DB Models
+class Todo(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    content = db.Column(db.Text, nullable=False)
+
+    def __str__(self):
+        return f'{self.id} {self.content}'
 
 @app.route('/api', methods=['GET'])
 def index():
